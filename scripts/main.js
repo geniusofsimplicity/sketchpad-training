5c7a16b192c9a0640a44c93ad5595ddcf6897343
geniusofsimplicity <genius.of.simplicity@gmail.com>
2016-08-03 14:52:21 +0200

    initial setup

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	var $container = $("#container");
+	createDiv($container);
+
+
+});
+
+function createGridV1($parent, size){
+	for (var i = 0; i < size; i++) {
+		for (var j = 0; j < size; j++) {
+			
+		};
+	};
+};
+
+function createDiv($parent){
+	$parent.append($("<div></div>"));
+};
